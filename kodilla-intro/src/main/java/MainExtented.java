2b0efc4c8b6ac09301be0734eb594b6536276afe
Magdalena Rudnik <mcabaj1989@gmail.com>
2020-05-23 21:10:39 +0200

    Initial version

---
+++ kodilla-intro/src/main/java/MainExtented.java
@@ -0,0 +1,16 @@
+public class MainExtented {
+    public static void main(String[] args) {
+        String mySubname = "Rudnik";
+        int myAge = 30;
+        double weight = 55;
+        boolean haveKids = true;
+        String nameKids = "Emilia";
+        int kidsAge = 4;
+        System.out.println(myAge);
+        System.out.println(mySubname);
+        System.out.println(weight);
+        System.out.println(haveKids);
+        System.out.println(nameKids);
+        System.out.println(kidsAge);
+    }
+}
