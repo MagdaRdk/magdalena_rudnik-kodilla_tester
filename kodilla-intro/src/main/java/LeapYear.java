4aed949578e85d697a6907602a9e0af98c82ee27
Magdalena Rudnik <mcabaj1989@gmail.com>
2020-06-01 21:13:14 +0200

    Initial version

--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class LeapYear {
+    public static void main(String [] args) {
+        int year = 2020;
+        System.out.println(year);
+        if (year%4 == 0 && year%100 != 0 || year%400 == 0) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("not leap");
+        }
+    }
 }
+
