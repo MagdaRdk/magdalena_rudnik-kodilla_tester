f1eba5f2e2e74c7ea2057bf7ea3b7b0dc0ee6282
Magdalena Rudnik <mcabaj1989@gmail.com>
2020-06-04 21:59:23 +0200

    0.11 Leap Year

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,6 +1,13 @@
 public class FirstClass {
+    static int suma(int a, int b) {
+
+        return a + b;
+    }
+
     public static void main(String[] args) {
         String text = "Hello from FirstClass!";
         System.out.println(text);
+
+        System.out.println(suma (2,4));
     }
 }
--- kodilla-intro/src/main/java/LeapYear.java
+++ kodilla-intro/src/main/java/LeapYear.java
@@ -1,12 +1,25 @@
 public class LeapYear {
-    public static void main(String [] args) {
-        int year = 2020;
+    public static void main(String[] args) {
+        int year = 2000;
         System.out.println(year);
-        if (year%4 == 0 && year%100 != 0 || year%400 == 0) {
+        if (isLeap(year)) {
             System.out.println("Leap Year");
         } else {
             System.out.println("not leap");
         }
+    }
+
+    private static boolean isLeap(int year) {
+        if (year % 4 != 0)
+            return false;
+        else if (year % 100 == 0)
+            return true;
+        else return year % 400 != 0;
+        /*if (year%4 == 0 && year%100 != 0 || year%400 == 0) {
+            System.out.println("Leap Year");
+        } else {
+            System.out.println("not leap");
+        }*/
     }
 }
 
