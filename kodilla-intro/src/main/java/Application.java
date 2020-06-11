afda33491ee62757f269b0a9bbddb411feb35742
Magdalena Rudnik <mcabaj1989@gmail.com>
2020-06-11 22:06:16 +0200

    2.6 Application

---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,27 @@
+public class Application {
+    String name;
+    double age;
+    double height;
+
+    public Application(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public void check() {
+        if (this.name != null) {
+            if (this.age > 30 && this.height > 160) {
+                System.out.println("User is older than 30 and higher then 160cm");
+            } else {
+                System.out.println("User is younger than 30 or lower than 160cm");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Application application = new Application("Adam", 40.5, 178);
+        application.check();
+    }
+}
+
