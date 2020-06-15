ce67c6dc201dc726140a91d9dbadd6523194da1a
Magdalena Rudnik <mcabaj1989@gmail.com>
2020-06-15 22:05:13 +0200

    2.4 Grades

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,57 @@
+public class Grades {
+
+    private int[] grades;
+    private int size;
+    private int value;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+        this.value = 0;
+    }
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+       this.grades[this.size] = value;
+        this.size++;
+    }
+
+    public static void main(String[] args) {
+
+        Grades grades = new Grades();
+        grades.getLastGrades();
+        grades.getAverage();
+
+        System.out.println(grades.getLastGrades());
+        System.out.println(grades.getAverage());
+
+        grades.add(5);
+        grades.add(4);
+        grades.add(3);
+        grades.add(4);
+        grades.add(4);
+        grades.add(3);
+        grades.add(2);
+        grades.add(3);
+        grades.add(5);
+        grades.add(5);
+
+
+    }
+    //public int getLastGrades(){//
+
+    }
+
+    public double getAverage() {
+        int result = 0;
+        double average;
+        int gradesNumber = grades.length;
+        for (int i = 0; i <= 10; i++) {
+            result = result + gradesNumber;
+        }
+        average = result / grades.length;
+      }
+        return average;
+
+}
