7bed3440213b0c4f3602a0f15cfb8c4964a060b4
KDRZAZGA <krzysztof.drzazga@op.pl>
2020-06-28 10:21:33 +0200

    4.3. review

--- kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/lists/homework/CarsListApplication.java
@@ -19,13 +19,13 @@ public class CarsListApplication {
         cars.add(new Ford(55.0));
 
         for (Car car : cars)
-            System.out.println(car);
+            System.out.println(car);/*TODO Review kdrzazga - Pls use describeCar() here*/
 
       cars.remove(3);
       cars.remove(opel);
 
         for (Car car : cars)
-            System.out.println(car);
+            System.out.println(car);/*TODO Review kdrzazga - Pls use describeCar() here*/
 
         System.out.println(cars.size());
 
