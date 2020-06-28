1b884f83c3b320f139f4f2205ce68ee06962e2b4
KDRZAZGA <krzysztof.drzazga@op.pl>
2020-06-28 10:17:30 +0200

    4.2. review

--- kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
@@ -11,8 +11,10 @@ public class CarsApplication {
 
     public static void main(String[] args) {
         Car[] cars = new Car[15];
+        /*TODO: Review kdrzazga Random size 1 to 15 is needed, as excercise says:
+         *  tablica o losowym rozmiarze od 1 do 15 samochodów.*/
         for (int n = 0; n < cars.length; n++)
-        cars[n] = drawCar();
+            cars[n] = drawCar();
         for (Car car : cars)
             CarUtils.describeCar(car);
     }
@@ -20,9 +22,9 @@ public class CarsApplication {
     public static Car drawCar() {
         Random random = new Random();
         int drawnCarKind = random.nextInt(3);
-        double a = random.nextDouble()* 100 + 1;
-        double b = random.nextDouble()* 100 + 1;
-        double c = random.nextDouble()* 100 + 1;
+        double a = random.nextDouble() * 100 + 1;
+        double b = random.nextDouble() * 100 + 1;/*TODO Review kdrzazga: Redunant variables. Please leave only variable a and use it in every case*/
+        double c = random.nextDouble() * 100 + 1;
         if (drawnCarKind == 0)
             return new Opel(a);
         else if (drawnCarKind == 1)
