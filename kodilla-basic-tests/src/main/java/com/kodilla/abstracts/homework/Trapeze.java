2cc07422ca35371401a02e21ceb957212a6811ef
KDRZAZGA <krzysztof.drzazga@op.pl>
2020-06-30 18:16:55 +0200

    3.2 corrected

--- kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Trapeze.java
+++ kodilla-basic-tests/src/main/java/com/kodilla/abstracts/homework/Trapeze.java
@@ -5,7 +5,6 @@ public class Trapeze extends Shape {
     int topBase;
     int bottomBase;
 
-
     public Trapeze(int sideHeight, int topBase, int bottomBase) {
         super(sideHeight);
         this.topBase = topBase;
@@ -19,8 +18,10 @@ public class Trapeze extends Shape {
 
     @Override
     public void circuit() {
-        System.out.println("Circuit is " + getSideHeight() + topBase + bottomBase + Math.sqrt((getSideHeight()*getSideHeight()) + ((bottomBase-topBase)*(bottomBase-topBase))));
-        }
+        double x = Math.abs(topBase - bottomBase) / 2.0;
+        double side = Math.sqrt(getSideHeight() * getSideHeight() + x * x);
+        System.out.println("Circuit is " + topBase + bottomBase + 2 * side);
+    }
 
 }
 
