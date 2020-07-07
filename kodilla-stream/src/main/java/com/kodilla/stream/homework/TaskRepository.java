9e75553551b11595791a4c19f990b5214f4f4e3d
Magdalena Rudnik <mcabaj1989@gmail.com>
2020-07-07 23:29:44 +0200

    6.1. Task - Stream

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/homework/TaskRepository.java
@@ -0,0 +1,16 @@
+package com.kodilla.stream.homework;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskRepository {
+    public static List<Task> getTask() {
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(new Task("go to the dentists", LocalDate.now(), LocalDate.of(2020, 07, 25)));
+        tasks.add(new Task("buy a medicine", LocalDate.now(), LocalDate.of(2020, 07, 28)));
+        tasks.add(new Task("go to the cinema", LocalDate.now(), LocalDate.of(2020, 07, 15)));
+        tasks.add(new Task("wash car", LocalDate.of(2020, 05, 15), LocalDate.of(2020, 06, 05)));
+        return tasks;
+    }
+}
