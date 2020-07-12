7248d1563a6d622a87681f436e9cd4b4eea1cf4e
KDRZAZGA <krzysztof.drzazga@op.pl>
2020-07-12 21:36:46 +0200

    6.2 - Optional hints

--- kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -7,20 +7,19 @@ import java.util.Optional;
 public class Application {
     public static void main(String[] args) {
 
-       List<Student> students = new ArrayList<>();
-        students.add(new Student("Jan Kowalski", " "));// jak podać tu nazwisko nauczyciela?
+        List<Student> students = new ArrayList<>();
+        students.add(new Student("Jan Kowalski", new Teacher("Kowalski")));// jak podać tu nazwisko nauczyciela?
         students.add(new Student("Marek Nowak", null));
-        students.add(new Student("Ania Machnik", " "));
+        students.add(new Student("Ania Machnik", new Teacher("Nowak")));
         students.add(new Student("Amelia Kozioł", null));
 
+        Optional<List<Student>> optionalStudent = Optional.ofNullable(students);//TODO Review - not needed
 
-        Optional<List<Student>> optionalStudent = Optional.ofNullable(students);
-
-       for (Student student : students) {
-            if (optionalStudent== null)
-                System.out.println("Student " + getName + "Teacher <undefined>)
-                        else
-                System.out.println("Student" + getName + "Teacher" + getName);
-                   } // czy zarys tej pętli jest poprawny?
+        for (Student student : students) {
+            System.out.println(Optional
+                    .ofNullable(student.getTeacher()) //check if teacher == null
+                    .map(Teacher::getName) //if Teacher exists - get his name
+                    .orElse("<undefined>")); //else - undefined
+        }
     }
 }
