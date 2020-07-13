53c3558b4a8f9dba8811ed9fb4ce4973b224b82c
Magdalena Rudnik <mcabaj1989@gmail.com>
2020-07-13 15:54:38 +0200

    6.2 Optional again

--- kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
+++ kodilla-stream/src/main/java/com/kodilla/optional/homework/Application.java
@@ -8,18 +8,16 @@ public class Application {
     public static void main(String[] args) {
 
         List<Student> students = new ArrayList<>();
-        students.add(new Student("Jan Kowalski", new Teacher("Kowalski")));// jak podać tu nazwisko nauczyciela?
+        students.add(new Student("Jan Kowalski", new Teacher("Zbigniew Kowalski")));
         students.add(new Student("Marek Nowak", null));
-        students.add(new Student("Ania Machnik", new Teacher("Nowak")));
+        students.add(new Student("Ania Machnik", new Teacher("Adrian Nowak")));
         students.add(new Student("Amelia Kozioł", null));
 
-        Optional<List<Student>> optionalStudent = Optional.ofNullable(students);//TODO Review - not needed
-
-        for (Student student : students) {
-            System.out.println(Optional
-                    .ofNullable(student.getTeacher()) //check if teacher == null
-                    .map(Teacher::getName) //if Teacher exists - get his name
-                    .orElse("<undefined>")); //else - undefined
+       for (Student student : students) {
+            System.out.println("Uczeń: " + student.getName() + " Nauczyciel: " + Optional
+                    .ofNullable(student.getTeacher())
+                    .map(Teacher::getName)
+                    .orElse("<undefined>"));
         }
     }
 }
