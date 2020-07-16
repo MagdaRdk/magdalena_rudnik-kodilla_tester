695b86ca2e1134d8b01174e923d4070e4cd78831
Magdalena Rudnik <mcabaj1989@gmail.com>
2020-07-16 20:59:35 +0200

    6.5 warehouseTestSuite again

--- kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/exception/homework/WarehouseTestSuite.java
@@ -5,24 +5,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class WarehouseTestSuite {
-   @Test
-    public void testGetOrder() throws OrderDoesntExistException {
-        // given
-        Warehouse warehouse = new Warehouse();
-        // when
-       Order isOrderNumberExist = warehouse.getOrder("103");
-        // then
-       assertNull(isOrderNumberExist); //why the test is failed? -> because Java compiler doesn´t even reach this line, Execution stops when Exception is thrown in getOrder
-       //TODO Review -> remove this test, testGetOrder_withException() does the job
-
-    }
 
     @Test(expected = OrderDoesntExistException.class)
     public void testGetOrder_withException() throws OrderDoesntExistException {
         // given
         Warehouse warehouse = new Warehouse();
         // when
-        Order isOrderNumberExist = warehouse.getOrder("104");
+        Order isOrderNumberExist = warehouse.getOrder("103");
         // then
         assertNotNull(isOrderNumberExist);
 
